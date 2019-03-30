9743669a7c63ff1ba70662092db5e621e121b2df
Wildemar Elias <wildemar-elias@live.com>
2019-03-30 14:25:54 -0300

    criamos a classe Usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,3 +1,72 @@
 <?php 
 
+class Usuario{
+	private $idusuario;
+	private $deslogin;
+	private $dessenha;
+	private $dtcadastro;
+
+//fazendo Getters and Setters
+	public function getIdusuario(){
+		return $this->idusuario;
+	}
+
+	public function setIdsuario($value){
+		$this->idusuario = $value;
+	}
+
+	public function getDesLogin(){
+		return $this->deslogin;
+	}
+
+	public function setDesLogin($value){
+		$this->deslogin = $value;
+	}
+
+	public function getDesSenha(){
+		return $this->dessenha;
+	}
+
+	public function setDesSenha($value){
+		$this->dessenha = $value;
+	}
+
+	public function getDtCadastro(){
+		return $this->dtcadastro;
+	}
+
+	public function setDtCadastro($value){
+		$this->dtcadastro = $value;
+	}
+
+	public function loadById($id){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",array(
+			":ID"=>$id
+		));
+
+		//carrega os dados do banco pro objeto
+		if(count($results)>0){
+			$row = $results[0];
+			$this->setIdsuario($row['idusuario']);
+			$this->setDesLogin($row['deslogin']);
+			$this->setDesSenha($row['dessenha']);
+			$this->setDtCadastro(new DateTime($row['dtcadastro']));
+		}
+	}
+
+	public function __toString(){
+		return json_encode(array(
+			"idusuario"=>$this->getIdusuario(),
+			"deslogin"=>$this->getDesLogin(),
+			"dessenha"=>$this->getDesSenha(),
+			"dtcadastro"=>$this->getDtCadastro()->format("d/m/Y H:i:s")
+		));
+	}
+
+}
+
+
+
+
  ?>
